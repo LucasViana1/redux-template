27beedbab17295e72dffd8efd3061783b58069e9
Lucas Cunha <you@example.com>
2021-12-17 00:59:35 -0300

    integrate the redux on Product component

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,11 +1,28 @@
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import './styles.css';
+import { addToCart, increaseQuantity } from '../../store/modules/cart/actions';
 
-const Product = () => (
-  <div className="product">
-    <b>Nome</b>
-    <p>Quantidade: 0</p>
-    <button type="button">+</button>
-  </div>
-);
+const Product = ({ product }) => {
+  const dispatch = useDispatch();
+  const { productsInCart } = useSelector((state) => state.cart);
+  const existInCart = productsInCart.find((productInCart) => productInCart.id === product.id);
+
+  return (
+    <div className="product">
+      <b>{product.name}</b>
+      <button type="button" onClick={() => dispatch(existInCart ? increaseQuantity(product) : addToCart(product))}>
+        Adicionar
+      </button>
+    </div>
+  );
+};
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Product;
