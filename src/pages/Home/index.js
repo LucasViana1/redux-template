328d1be00e185b1284d03a0c475c97f06a38c463
Lucas Cunha <you@example.com>
2021-12-17 00:55:31 -0300

    remove redux on Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,14 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import Header from '../../components/Header';
 import Products from '../../components/Products';
 import Summary from '../../components/Summary';
 import './styles.css';
-import { productsLoadRequest } from '../../store/modules/products/actions';
 
-const Home = () => {
-  const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.products);
-
-  useEffect(() => {
-    dispatch(productsLoadRequest());
-  }, [dispatch]);
-
-  console.log(products);
-  return (
-    <main className="home">
-      <Header />
-      <Products />
-      <Summary />
-    </main>
-  );
-};
+const Home = () => (
+  <main className="home">
+    <Header />
+    <Products />
+    <Summary />
+  </main>
+);
 
 export default Home;
