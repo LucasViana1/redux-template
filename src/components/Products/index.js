59789f66716c8b89ab347be848b14d38ef54bdb0
Lucas Cunha <you@example.com>
2021-12-17 00:59:18 -0300

    fetch products on component

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,14 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../Product';
+import { productsLoadRequest } from '../../store/modules/products/actions';
+
 import './styles.css';
 
-const Products = () => (
-  <section className="products">
-    <h2>Lista de produtos:</h2>
-    <Product />
-    <Product />
-    <Product />
-    <Product />
-  </section>
-);
+const Products = () => {
+  const dispatch = useDispatch();
+  const { products, loading } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(productsLoadRequest());
+  }, [dispatch]);
+
+  return (
+    <section className="products">
+      <h2>Lista de produtos:</h2>
+      {loading && <strong>Carregando...</strong>}
+      {!loading && products && products.map((product) => <Product key={product.id} product={product} />)}
+    </section>
+  );
+};
 
 export default Products;
