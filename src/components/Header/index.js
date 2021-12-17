01488714ae47044cdaa635c5773eeedba150a254
Lucas Cunha <you@example.com>
2021-12-17 00:54:43 -0300

    integrate the redux on Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,15 @@
+import { useSelector } from 'react-redux';
 import './styles.css';
 
-const Header = () => (
-  <header className="header">
-    <div>Carrinho: 0</div>
-  </header>
-);
+const Header = () => {
+  const { productsInCart } = useSelector((state) => state.cart);
+  const total = productsInCart.reduce((acc, product) => acc + product.quantity, 0);
+
+  return (
+    <header className="header">
+      <div>Carrinho: {total}</div>
+    </header>
+  );
+};
 
 export default Header;
