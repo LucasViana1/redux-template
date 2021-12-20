6bd9c04cd2032610f0d87ace3c1619a6f172b6da
Lucas Cunha <you@example.com>
2021-12-20 10:17:44 -0300

    create Products with class component

---
+++ src/components/Products/class.js
@@ -0,0 +1,40 @@
+import { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import Product from '../Product';
+import { productsLoadRequest } from '../../store/modules/products/actions';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+class Products extends PureComponent {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    products: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(productsLoadRequest());
+  }
+
+  render() {
+    const { products, loading } = this.props;
+
+    return (
+      <section className="products">
+        <h2>Lista de produtos:</h2>
+        {loading && <strong>Carregando...</strong>}
+        {!loading && products && products.map((product) => <Product key={product.id} product={product} />)}
+      </section>
+    );
+  }
+};
+
+const mapStateToProps = (state) => ({
+  products: state.products.products,
+  loading: state.products.loading,
+});
+
+const mapDispatch = (dispatch) => ({ dispatch });
+
+export default connect(mapStateToProps, mapDispatch)(Products);
